7d3e0f23ad12d061cff1a761cc86ca281720528a
Aliaksandr <djvalentinoff@gmail.com>
2023-09-21 12:36:40 +0300

    add: select logic at the loading

--- src/shared/Header/SortBlock/SortBlock.tsx
+++ src/shared/Header/SortBlock/SortBlock.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import styles from './sortblock.css';
-import { RocketIcon } from '../../Icons';
-
+import React from "react";
+import styles from "./sortblock.css";
+import { RocketIcon } from "../../Icons";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/reducer";
 
 export function SortBlock() {
+  const token = useSelector<RootState>((state) => state.token);
+
   return (
     <div className={styles.sortBlock}>
-      <RocketIcon />
-      <select className={styles.sortSelect}>
-        <option>Лучшие</option>
-        <option>По автору</option>
-        <option>По дате</option>
-      </select>
+      {token !== 'undefined' ? (
+        <>
+          <RocketIcon />
+          <select className={styles.sortSelect}>
+            <option>Лучшие</option>
+            <option>По автору</option>
+            <option>По дате</option>
+          </select>
+        </>
+      ) : null}
     </div>
   );
 }
