eeae6fde5134d00fe65a95dc76f5b9dabc6b475d
Aliaksandr <djvalentinoff@gmail.com>
2023-09-21 12:14:51 +0300

    add: select sort header

--- src/shared/Header/SortBlock/SortBlock.tsx
+++ src/shared/Header/SortBlock/SortBlock.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './sortblock.css';
+import { RocketIcon } from '../../Icons';
+
 
 export function SortBlock() {
   return (
     <div className={styles.sortBlock}>
-      sorting dropdown
+      <RocketIcon />
+      <select className={styles.sortSelect}>
+        <option>Лучшие</option>
+        <option>По автору</option>
+        <option>По дате</option>
+      </select>
     </div>
   );
 }
